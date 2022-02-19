32c1c63ef8e485747048ad5fd1d13cfc1bc9c0e0
Kush-Bilakhia <94743789+Kush-Bilakhia@users.noreply.github.com>
2022-02-19 15:08:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var first_name="Ranbir";
+var second_name="Kapoor";
+var full_name= first_name.concat(second_name);
+console.log(full_name);
+var length_name=full_name.length;
+console.log(length_name);
+var replace_name=full_name.replace("Ranbir","Anil");
+console.log(replace_name)
+var making_lower_case=first_name.toLowerCase()
+console.log(making_lower_case)
+var making_upper_case=second_name.toUpperCase()
+console.log(making_upper_case)
+var square_root=Math.sqrt(225)
+console.log(square_root)
+var max_num=Math.max(92,91)
+console.log(max_num)
+var min_num=Math.min(92,91)
+console.log(min_num)
+var power=Math.pow(4,2)
+console.log(power)
